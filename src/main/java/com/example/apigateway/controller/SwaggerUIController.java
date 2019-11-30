809702d0f315c8443cf85979e4059d6cf4761b11
Nate Tuvera <ntuvera@gmail.com>
2019-11-29 20:32:53 -0500

    DONE: apigateway customUserServiceTest: 2/3 passing

---
+++ src/main/java/com/example/apigateway/controller/SwaggerUIController.java
@@ -0,0 +1,12 @@
+//package com.example.apigateway.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class SwaggerUIController {
+//    @RequestMapping(value = "/")
+//    public String index() {
+//        return "redirect:swagger-ui.html";
+//    }
+//}
