bef0c430220ccfd478d1ae93d56ea7b45b388fbf
Nate Tuvera <ntuvera@gmail.com>
2019-12-04 11:29:13 -0500

    Edge of coverage

--- src/test/java/com/example/apigateway/ZuulGatewayApplicationTests.java
+++ src/test/java/com/example/apigateway/ZuulGatewayApplicationTests.java
@@ -2,10 +2,6 @@ package com.example.apigateway;
 
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-
-@SpringBootTest
-@EnableSwagger2WebMvc
 class ZuulGatewayApplicationTests {
 
 	@Test
